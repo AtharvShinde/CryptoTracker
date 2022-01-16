041b150c887b0b59c66893142704918f2dec7862
Atharv Shinde <atharvshinde@Atharvs-MacBook-Air.local>
2022-01-16 16:48:28 +0530

    Updates

---
+++ precache-manifest.f047d47700f096097ce61486656d637f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993ecf65e0723c5ff61f7ef07cf6e875",
+    "url": "/CryptoTracker/index.html"
+  },
+  {
+    "revision": "80ac589f51494dd65b77",
+    "url": "/CryptoTracker/static/css/main.5ee6d9a3.chunk.css"
+  },
+  {
+    "revision": "1c59757a6e0b05e18346",
+    "url": "/CryptoTracker/static/js/2.db32c46c.chunk.js"
+  },
+  {
+    "revision": "80ac589f51494dd65b77",
+    "url": "/CryptoTracker/static/js/main.6192d560.chunk.js"
+  },
+  {
+    "revision": "3ffb6c1309b175adff79",
+    "url": "/CryptoTracker/static/js/runtime~main.28eecf5a.js"
+  }
+]);
